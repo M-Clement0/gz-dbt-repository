3a9944c4398ad275bd453d199c863c44f966b51f
ludovicclement30@gmail.com <ludovicclement30@gmail.com>
2024-10-30 16:24:59 +0000

    Creating finance campaign mart model

---
+++ models/mart/finance/finance_campaigns_day.sql
@@ -0,0 +1,19 @@
+SELECT
+    date_date   
+    ,ROUND((operational_margin - ads_cost),2) AS ads_margin
+    ,average_basket
+    ,operational_margin
+    ,c.ads_cost
+    ,c.ads_impression
+    ,c.ads_click
+    ,f.quantity
+    ,f.revenue
+    ,f.purchase_cost
+    ,f.margin
+    ,f.shipping_fee
+    ,f.logcost
+    ,f.ship_cost
+FROM {{ ref('int_campaigns_day') }} c
+LEFT JOIN {{ ref('finance_days') }} f
+    Using(date_date)
+ORDER BY date_date DESC
