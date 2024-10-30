e2b2f274611dc6e148792e8e749e5351ff7fe7c6
ludovicclement30@gmail.com <ludovicclement30@gmail.com>
2024-10-30 08:59:59 +0000

    Orders operational modified

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,7 +1,15 @@
-SELECT  orders_id
-    ,date_date
-    ,ROUND(((margin + shipping_fee) - logcost - ship_cost ),2) AS Operational_margin
-    ,quantity
-FROM {{ ref('int_orders_margin') }}
-LEFT JOIN {{ ref('stg_raw__ship') }}
-    Using (orders_id)
+ SELECT
+    o.orders_id
+     ,o.date_date
+     ,ROUND(o.margin + s.shipping_fee - (s.logcost + s.ship_cost),2) AS operational_margin
+     ,o.quantity
+     ,o.revenue
+     ,o.purchase_cost
+     ,o.margin
+     ,s.shipping_fee
+     ,s.logcost
+     ,s.ship_cost
+ FROM {{ref("int_orders_margin")}} o
+ LEFT JOIN {{ref("stg_raw__ship")}} s
+     USING(orders_id)
+ ORDER BY orders_id desc
